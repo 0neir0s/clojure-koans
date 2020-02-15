c1780bbca41fab3e5516abfc70490f01b1d084c2
0neir0s <athular7@gmail.com>
2020-02-15 17:13:23 +0530

    14th one done

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,29 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [subseq coll
+         reversed-seq '()]
+    (if (= (count subseq) 0)
+      reversed-seq
+      (recur (next subseq) (conj reversed-seq (first subseq))))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if(= n 1)
+      acc
+      (recur (- n 1) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
@@ -51,4 +59,4 @@
   (< 1000000000000000000000000N (factorial 1000N))
 
   "But what happens when the machine limits you?"
-  (< 1000000000000000000000000N (factorial 100003N)))
+  ;(< 1000000000000000000000000N (factorial 100003N)))
